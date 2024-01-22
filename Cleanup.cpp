1aa423b662a26b518cd56ba2cc7b49546ceb3ee8
Mister1SI <wazzup2690@gmail.com>
2024-01-22 10:19:13 -0800

    Set up fixed pipeline state

--- Cleanup.cpp
+++ Cleanup.cpp
@@ -1,15 +1,15 @@
 #include "Volken.h"
 
 void Volken::cleanup() {
+
+
 	if (enableValidationLayers) {
 		DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
 	}
 
 
-	for (auto imageView : imageViews) {
-		vkDestroyImageView(device, imageView, nullptr);
-	}
-
+	vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+	for (auto imageView : imageViews) { vkDestroyImageView(device, imageView, nullptr); }
 	vkDestroySwapchainKHR(device, swapchain, nullptr);
 	vkDestroyDevice(device, nullptr);
 	vkDestroySurfaceKHR(instance, surface, nullptr);
--- Volken.h
+++ Volken.h
@@ -35,7 +35,20 @@ struct SwapChainSupportDetails {
 	std::vector<VkPresentModeKHR> presentModes;
 };
 
+static std::vector<char> readFile(const std::string& filename) {
+	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+	if (!file.is_open()) {
+		throw std::runtime_error("failed to open file!");
+	}
 
+	size_t fileSize = (size_t)file.tellg();
+	std::vector<char> buffer(fileSize);
+
+	file.seekg(0);
+	file.read(buffer.data(), fileSize);
+	file.close();
+	return buffer;
+}
 
 class Volken {
 private:
@@ -76,25 +89,6 @@ private:
 	VkShaderModule createShaderModule(const std::vector<char>& code);
 
 
-	static std::vector<char> readFile(const std::string& filename) {
-		std::ifstream file(filename, std::ios::ate | std::ios::binary);
-		if (!file.is_open()) {
-			throw std::runtime_error("failed to open file!");
-		}
-
-		size_t fileSize = (size_t)file.tellg();
-		std::vector<char> buffer(fileSize);
-
-		file.seekg(0);
-		file.read(buffer.data(), fileSize);
-
-		file.close();
-
-		return buffer;
-	}
-
-
-
 	// Debug
 	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
 			VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
@@ -127,7 +121,7 @@ private:
 	VkFormat swapchainImageFormat;
 	VkExtent2D swapchainExtent;
 	std::vector<VkImageView> imageViews;
-
+	VkPipelineLayout pipelineLayout;
 
 
 	std::vector<const char*> validationLayers = {
@@ -138,6 +132,10 @@ private:
 		VK_KHR_SWAPCHAIN_EXTENSION_NAME
 	};
 
+	std::vector<VkDynamicState> dynamicStates = {
+		VK_DYNAMIC_STATE_VIEWPORT,
+		VK_DYNAMIC_STATE_SCISSOR
+	};
 
 
 public:
